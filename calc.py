2265314509ed8ad764077fd92c87d2fc2009f2ee
Mixy <streamix15@gmail.com>
2021-12-22 15:50:32 +0700

    menambahkan fungsi sub()

--- calc.py
+++ calc.py
@@ -1,6 +1,11 @@
 def add(a,b):
 	return a+b
 
+def sub(a,b):
+	return a-b
+
 if __name__ == "__main__":
 	c = add(5,2)
-	print(c)
+	print("Addition 5 + 2 = "+str(c))
+	print("Subtraction 5-2 = "+str(sub(5,2)))
+	
